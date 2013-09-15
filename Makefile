6090f70fa8b7cf3b19693ad77e0ec1ba3171eb2f
Josh Chisholm <joshuachisholm@gmail.com>
2013-09-15 12:16:53 +0100

    mechanize switch (cukes with js on/off)

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	MECHANIZE=true cucumber && cucumber
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,17 @@
 require 'cappie'
 
+driver = :selenium
+
+if ENV["MECHANIZE"] == "true"
+  require 'capybara/mechanize'
+  driver = :mechanize
+  Capybara.app = true
+end
+
 Cappie.start(
   command: './node_modules/.bin/pogo server.pogo',
   await: /listening at 4647/,
   host: 'http://localhost:4647',
-  driver: :selenium,
+  driver: driver,
   environment: { PORT: '4647' }
 )
